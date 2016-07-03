d9e640ec88d022a8ea33a9dc1afbfdd569364440
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-03 14:54:15 +0200

    postgresql

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,22 @@ var dotenv = require('dotenv');
 
 dotenv.load();
 
-module.exports = {
-  client: 'sqlite',
-  connection: {
-    filename: './dev.sqlite3'
-  },
-  useNullAsDefault: true
-};
+if(process.env.POSTGRESQL){
+  module.exports = {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    }
+  };
+}else {
+  module.exports = {
+    client: 'sqlite',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullAsDefault: true
+  };
+}
