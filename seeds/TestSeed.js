72dbb04ee4b0e57c120aac8640a4d2375224c7da
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-23 18:46:57 +0200

    [seeds] simple seed for user

---
+++ seeds/TestSeed.js
@@ -0,0 +1,21 @@
+var crypto = require('crypto');
+var bcrypt = require('bcrypt-nodejs');
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('users').del(),
+    knex('ssh_keys').del(),
+
+    // Inserts seed entries
+    new Promise(function(resolve, reject) {
+      bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash('test', salt, null, function(err, hash) {
+          resolve(hash);
+        });
+      })
+    }).then(function(hash){
+      return knex('users').insert({id: 1, name: 'test', email:'test@test.com', password:hash});
+    })
+  );
+};
