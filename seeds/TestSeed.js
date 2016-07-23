1e0ba131381bd48704af92ff364ae385b05ba3bd
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-23 18:51:24 +0200

    [seeds] simple key inserted in seed

--- seeds/TestSeed.js
+++ seeds/TestSeed.js
@@ -16,6 +16,8 @@ exports.seed = function(knex, Promise) {
       })
     }).then(function(hash){
       return knex('users').insert({id: 1, name: 'test', email:'test@test.com', password:hash});
-    })
+    }),
+
+    knex('ssh_keys').insert({id: 1, user_id:1, name: 'test_key', private:'privateKEY', public:'PUBLICKEY'})
   );
 };
