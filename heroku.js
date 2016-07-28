f5bdc40434face1f827e140f62827994621bc8ca
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-07-28 02:32:06 +0200

    [heroku] try to fix nodegit problems

--- heroku.js
+++ heroku.js
@@ -9,4 +9,4 @@ try {
     // Query the entry
     stats = fs.lstatSync('/app/.apt/usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.20');
     fs.renameSync("/app/.apt/usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.20", "/app/.apt/usr/lib/x86_64-linux-gnu/libstdc++.so.6");
-}
+}catch(e){}
